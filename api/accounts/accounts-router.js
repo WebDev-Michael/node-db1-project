926ceebb5954c05a2c0cb6552b568a9807194702
Krygore <webdev.krygore@gmail.com>
2023-04-08 22:04:16 -0700

    making progress

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,48 @@
 const router = require('express').Router()
+const middleware = require('./accounts-middleware')
+const Account = require('./accounts-model')
 
-router.get('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/', async (req, res, next) => {
+try {
+  const accounts = await Account.getAll()
+  res.json(accounts)
+} catch (err) {
+  next(err)
+}
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.get('/:id', middleware.checkAccountId, async (req, res, next) => {
+  res.json(req.account)
 })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
+router.post('/', middleware.checkAccountPayload, middleware.checkAccountNameUnique, (req, res, next) => {
+  try {
+    res.json('post account')
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.put('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.put('/:id', middleware.checkAccountId, middleware.checkAccountPayload, middleware.checkAccountNameUnique, (req, res, next) => {
+  try {
+    res.json('update account')
+  } catch (err) {
+    next(err)
+  }
 });
 
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+router.delete('/:id', middleware.checkAccountId, (req, res, next) => {
+  try {
+    res.json('delete account')
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.use((err, req, res, next) => { // eslint-disable-line
-  // DO YOUR MAGIC
+router.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+  })
 })
 
 module.exports = router;
